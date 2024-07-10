da4af93cc6db4594128baf20ff965a5ab6e52454
Sam Friedman <sam@golioth.io>
2024-07-10 16:43:19 -0400

    Add support for sending Command RPCs
    Does not include support for retries or handling errors at this point.
    
    Signed-off-by: Sam Friedman <sam@golioth.io>

---
+++ include/openmv_rpc.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2024 Golioth, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+
+int openmv_rpc_call(const char *command,
+                    const void *command_payload,
+                    size_t command_payload_len,
+                    void *result_payload,
+                    size_t result_payload_len);
+int openmv_rpc_send_stream(const char *method);
+int openmv_rpc_receive_stream(void);
